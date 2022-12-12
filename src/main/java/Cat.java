c01a5eed9828aa97dff05d987ce26abeca518297
AleksandrEzhov <104377724+AlexEzhov88@users.noreply.github.com>
2022-12-12 19:02:31 +0300

    bean
    Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    public String getMassage() {
+        return massage;
+    }
+
+    public void setMassage(String massage) {
+        this.massage = massage;
+    }
+
+    private String massage;
+}
